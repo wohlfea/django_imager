7759405f4fa5aa85331b0e8293b1c15a189a0dac
wohlfea <wohlfea@gmail.com>
2016-04-09 22:10:49 -0700

    added imager_profile model and tied class to User.

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ImagerProfile(models.Model):
+    """Profile attached to django's user model."""
+    user = models.OneToOneField(User, related_name='profile')
+    location = models.CharField(default='', max_length=255)
+    bio = models.TextField(default='')
+
+    @classmethod
+    def active(cls):
+        """Return profiles of all active users"""
+        return cls.objects.filter(is_active=True)
+
+    @property
+    def is_active(self):
+        """Return True if associated user is active."""
+        return self.user.is_active
