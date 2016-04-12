ef8ce8e7f51ab3c19ccfbc66e17eb82445fac8ec
wohlfea <wohlfea@gmail.com>
2016-04-11 19:46:32 -0700

    tests are functional for all models.

--- imagersite/imager_profile/apps.py
+++ imagersite/imager_profile/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class ImagerProfileConfig(AppConfig):
     name = 'imager_profile'
+
+    def ready(self):
+        from imager_profile import handlers
--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -12,13 +12,8 @@ class TestProfile(TestCase):
         self.test_user2 = User.objects.create_user('test user2',
                                                    'test2@test.com',
                                                    'testpassword2')
-        self.test_profile1 = ImagerProfile()
-        self.test_profile2 = ImagerProfile()
-
-        self.test_profile1.user = self.test_user1
-        self.test_profile2.user = self.test_user2
-        self.test_profile1.save()
-        self.test_profile2.save()
+        self.test_user1.save()
+        self.test_user2.save()
 
     def test_profile_exists(self):
         """Verify profile has been created."""
@@ -26,7 +21,8 @@ class TestProfile(TestCase):
 
     def test_profile_verify(self):
         """Verify profile is the expected profile."""
-        self.assertEquals(ImagerProfile.objects.all()[0], self.test_profile1)
+        self.assertEquals(ImagerProfile.objects.all()[0],
+                          self.test_user1.profile)
 
     def test_profile_user(self):
         """Test profile user is the expected user."""
@@ -43,14 +39,22 @@ class TestProfile(TestCase):
         """Test when profile is deleted, user still exists."""
         self.assertEquals(len(User.objects.all()), 2)
         self.assertEquals(len(ImagerProfile.objects.all()), 2)
-        self.test_profile2.delete()
+        self.test_user2.profile.delete()
         self.assertEquals(len(ImagerProfile.objects.all()), 1)
         self.assertEquals(len(User.objects.all()), 2)
 
     def test_profile_active(self):
         """Test profile is active."""
-        self.assertEquals(self.test_profile1.is_active, True)
+        self.assertEquals(self.test_user1.profile.is_active, True)
 
     def test_active_profiles(self):
         """Test full list of active profiles is returned."""
-        self.assertEquals(len(ImagerProfile.active()), 2)
+        self.assertEquals(ImagerProfile.active.count(), 2)
+
+    def test_friends(self):
+        """Test profiles can be given friends."""
+        p = self.test_user1.profile
+        p.friends.add(self.test_user2)
+        self.assertEquals(p.friends.all()[0], self.test_user2)
+        self.assertEquals(self.test_user2.friend_of.all()[0],
+                          self.test_user1.profile)
