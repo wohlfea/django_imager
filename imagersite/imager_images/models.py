3c2a3bee5bf2f1dec63054f918225746a68f0e13
wohlfea <wohlfea@gmail.com>
2016-04-17 15:37:36 -0700

    set up better relationship with foreignkey to stick around

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -25,6 +25,7 @@ class Album(models.Model):
     date_published = models.DateTimeField(auto_now_add=True)
     published = models.CharField(max_length=7, choices=VISIBILITY_CHOICES,
                                  default='Private')
+    cover = models.ForeignKey('Image', null=True, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
 
@@ -48,5 +49,3 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
-
-Album.cover = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
