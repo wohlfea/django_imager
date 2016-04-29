d1fe098d576da32b6821a7f27a6e52763ec6be04
paulsheridan <paul.sheridan@me.com>
2016-04-29 15:32:41 -0700

    removed template debug to get to the root of this 400

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -4,7 +4,7 @@ from .settings import *
 
 DEBUG = False
 THUMBNAIL_DEBUG = False
-TEMPLATE_DEBUG = False
+# TEMPLATE_DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
