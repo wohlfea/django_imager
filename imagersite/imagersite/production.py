8de78c0195a3d19cbdeff807b1966af1980047a8
paulsheridan <paul.sheridan@me.com>
2016-04-29 10:56:59 -0700

    updated git cache to ignore media dir

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -1,5 +1,6 @@
 import os
 import dj_database_url
+from .settings import *
 
 DEBUG = os.environ.get("DEBUG")
 THUMBNAIL_DEBUG = os.environ.get("DEBUG")
@@ -7,7 +8,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-ALLOWED_HOSTS = ['.us-west-2.compute.amazonaws.com', 'localhost']
+ALLOWED_HOSTS = [
+    '.us-west-2.compute.amazonaws.com',
+    'localhost',
+]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get("EMAIL_HOST ")
